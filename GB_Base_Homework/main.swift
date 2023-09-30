c6bcd54962e2680c978dd9e6c8521c9c71156b4a
DickobrazS <60858569+DickobrazS@users.noreply.github.com>
2023-09-30 21:48:28 +0300

    first commit (not full)

--- GB_Base_Homework/main.swift
+++ GB_Base_Homework/main.swift
@@ -7,5 +7,47 @@
 
 import Foundation
 
-print("Hello, World!")
+//MARK: Enums
+enum Status: String{
+    case closed = "закрыт"
+    case opened = "открыт"
+}
 
+//MARK: Protocols
+
+protocol CarStatus {
+    var doorsStatus: Status { get set }
+    var trunkStatus: Status { get set }
+    var centralLock: Status { get set }
+}
+
+//MARK: Classes
+
+class Car: CarStatus {
+    let carModel: String
+    let carColor: String
+    var doorsStatus: Status
+    var trunkStatus: Status
+    var centralLock: Status
+    var horsePower: Int
+    
+    init(carModel: String,
+         carColor: String,
+         doorsStatus: Status,
+         trunkStatus: Status,
+         centralLock: Status,
+         horsePower: Int)
+    {
+        self.carModel = carModel
+        self.carColor = carColor
+        self.doorsStatus = doorsStatus
+        self.trunkStatus = trunkStatus
+        self.centralLock = centralLock
+        self.horsePower = horsePower
+    }
+    
+}
+
+
+
+ 
